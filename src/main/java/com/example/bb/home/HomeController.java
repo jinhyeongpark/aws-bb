d79dd5a45bc73398b62288d4c30b5060af2166c5
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-01-29 02:50:04 +0900

    add home path

--- src/main/java/com/example/bb/home/HomeController.java
+++ src/main/java/com/example/bb/home/HomeController.java
@@ -1,14 +1,14 @@
-package com.example.bb.healthcheck;
+package com.example.bb.home;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthCheck {
+public class HomeController {
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "healthy";
+    @GetMapping("/")
+    public String home() {
+        return "hello";
     }
 
 }
